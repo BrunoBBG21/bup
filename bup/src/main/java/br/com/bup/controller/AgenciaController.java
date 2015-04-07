a82f2bb0dd811feaa6af0daae293190ce2db7a7f
andre luis tomaz dionisio <andreluisdionisio@gmail.com>
2015-04-07 15:00:11 -0300

    agencia controller

---
+++ bup/src/main/java/br/com/bup/controller/AgenciaController.java
@@ -0,0 +1,107 @@
+package br.com.bup.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.bup.annotation.OpenTransaction;
+import br.com.bup.dao.AgenciaDAO;
+import br.com.bup.dao.AnuncianteDAO;
+import br.com.bup.dao.LancesLeilaoDAO;
+import br.com.bup.dao.UsuarioDAO;
+import br.com.bup.domain.Agencia;
+import br.com.bup.domain.Anunciante;
+import br.com.bup.domain.Usuario;
+import br.com.bup.web.UsuarioSession;
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.SimpleMessage;
+import br.com.caelum.vraptor.validator.Validator;
+
+@Controller
+public class AgenciaController {
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(AgenciaController.class);
+
+	private final Result result;
+	private final Validator validator;
+	private final UsuarioSession usuarioSession;
+	private final UsuarioDAO usuarioDAO;
+
+	private final AgenciaDAO agenciaDAO;
+
+	private final LancesLeilaoDAO lancesLeilaoDAO;
+	
+	private final AnuncianteDAO anuncianteDAO;
+
+	/**
+	 * @deprecated CDI eyes only
+	 */
+	protected AgenciaController() {
+		this(null, null, null, null,null, null, null);
+	}
+
+	@Inject
+	public AgenciaController(Result result, Validator validator,
+			AgenciaDAO agenciaDAO,LancesLeilaoDAO lancesLeilaoDAO,AnuncianteDAO anuncianteDAO, UsuarioSession usuarioSession,UsuarioDAO usuarioDAO) {
+		this.result = result;
+		this.validator = validator;
+		this.agenciaDAO = agenciaDAO;
+		this.lancesLeilaoDAO = lancesLeilaoDAO;
+		this.anuncianteDAO = anuncianteDAO;
+		this.usuarioSession = usuarioSession;
+		this.usuarioDAO = usuarioDAO;
+	}
+
+	public void formulario() {
+		LOGGER.debug("carregando formulario de agencia");
+		result.include("lances", lancesLeilaoDAO.buscarTodos());
+		result.include("gerencias", anuncianteDAO.buscarTodos());
+		result.include("usuarios", usuarioDAO.buscarTodos());
+		// simples formulario... futuramente receendo id para editar... ou
+		// nao...
+	}
+
+	@OpenTransaction
+	public void criar(@NotNull String cnpj,@NotNull Usuario usuario,List<Anunciante> gerencias) {
+		validator.onErrorRedirectTo(this).formulario(); // caso seja null...
+		LOGGER.debug("criando agencia: agencia - " + cnpj+ ", usuario - "+usuario.getNome());
+		Usuario logado = usuarioSession.getUsuario();
+		if (!("admin".equals(logado.getNome()) && logado.getId() == 1)) {
+			validator.add(new SimpleMessage("error", "Usuario deve ser o administrador"));
+			validator.onErrorRedirectTo(this).formulario();
+		} else {
+			Agencia agencia = new Agencia();
+			agencia.setCnpj(cnpj);
+			agencia.setGerencia(gerencias);
+			agencia.setCep(usuario.getCep());
+			agencia.setEmail(usuario.getEmail());
+			agencia.setEndereco(usuario.getEndereco());
+			agencia.setNome(usuario.getNome());
+			agencia.setPassword(usuario.getPassword());
+			agencia.setSaldo(usuario.getSaldo());
+			agencia.setTelefone(usuario.getTelefone());
+			agencia.setLances(agencia.getLances());
+			agencia.setContasBancarias(usuario.getContasBancarias());
+			// validacoes...
+			validarCriar(agencia);
+			validator.onErrorRedirectTo(this).formulario();
+
+			// salva
+			agenciaDAO.salvar(agencia);
+
+			result.include("success", "conta bancaria criada com sucesso.");
+			result.redirectTo(IndexController.class).index();
+		}
+	}
+
+	private void validarCriar(Agencia agencia) {
+		validator.validate(agencia);
+
+		// TODO validar inclusao repetida
+	}
+}
