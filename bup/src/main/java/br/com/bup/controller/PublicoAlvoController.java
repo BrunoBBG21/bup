39c0b6258e831bf9e02ce263612a3e397df8296d
andre luis tomaz dionisio <andreluisdionisio@gmail.com>
2015-05-02 15:21:27 -0300

    controlador de publico alvo

---
+++ bup/src/main/java/br/com/bup/controller/PublicoAlvoController.java
@@ -0,0 +1,71 @@
+package br.com.bup.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.bup.annotation.OpenTransaction;
+import br.com.bup.dao.PublicoAlvoDAO;
+import br.com.bup.domain.PublicoAlvo;
+import br.com.bup.web.UsuarioSession;
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.Validator;
+@Controller
+public class PublicoAlvoController {
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(PublicoAlvoController.class);
+
+	private final Result result;
+	private final Validator validator;
+	private final PublicoAlvoDAO publicoAlvoDAO;
+	private final UsuarioSession usuarioSession;
+	
+	/**
+	 * @deprecated CDI eyes only
+	 */
+	protected PublicoAlvoController() {
+		this(null, null, null, null);
+	}
+	@Inject
+	public PublicoAlvoController(Result result, Validator validator,
+			PublicoAlvoDAO publicoAlvoDAO, 
+			UsuarioSession usuarioSession) {
+		this.result = result;
+		this.validator = validator;
+		this.publicoAlvoDAO = publicoAlvoDAO;
+		this.usuarioSession = usuarioSession;
+	}
+	@OpenTransaction
+	public void formulario() {
+		LOGGER.debug("carregando formulario de publico alvo.");
+	}
+	@OpenTransaction
+	public List<PublicoAlvo> listar() {
+		LOGGER.debug("Listando os públicos alvos. ");
+		
+		return publicoAlvoDAO.buscarTodos();
+	}
+	@OpenTransaction
+	public void criar(@NotNull PublicoAlvo publicoAlvo) {
+		validator.onErrorRedirectTo(this).formulario(); //caso seja null...
+		LOGGER.debug("criando publico alvo: " + publicoAlvo);
+		
+		//validacoes...
+		validarCriar(publicoAlvo);
+        validator.onErrorRedirectTo(this).formulario();		
+		
+        //salva
+        publicoAlvoDAO.salvar(publicoAlvo);
+		
+		result.include("success", "Modalidade de pagamento criada com sucesso.");
+		result.redirectTo(IndexController.class).index();
+	}
+	private void validarCriar(PublicoAlvo publicoAlvo) {
+		validator.validate(publicoAlvo);
+	}
+}
