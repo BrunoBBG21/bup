861578410321cb4d7b34414f180290927fa2f2bb
andre luis tomaz dionisio <andreluisdionisio@gmail.com>
2015-05-04 15:31:19 -0300

    i18n no publico alvo

--- bup/src/main/java/br/com/bup/controller/PublicoAlvoController.java
+++ bup/src/main/java/br/com/bup/controller/PublicoAlvoController.java
@@ -1,6 +1,7 @@
 package br.com.bup.controller;
 
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
@@ -16,6 +17,7 @@ import br.com.bup.web.UsuarioSession;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.core.JstlLocalization;
 import br.com.caelum.vraptor.validator.I18nMessage;
 import br.com.caelum.vraptor.validator.Validator;
 @Controller
@@ -27,21 +29,24 @@ public class PublicoAlvoController {
 	private final Validator validator;
 	private final PublicoAlvoDAO publicoAlvoDAO;
 	private final UsuarioSession usuarioSession;
-	
+	private final JstlLocalization local;
+	private final ResourceBundle i18n;
 	/**
 	 * @deprecated CDI eyes only
 	 */
 	protected PublicoAlvoController() {
-		this(null, null, null, null);
+		this(null, null, null, null,null);
 	}
 	@Inject
 	public PublicoAlvoController(Result result, Validator validator,
 			PublicoAlvoDAO publicoAlvoDAO, 
-			UsuarioSession usuarioSession) {
+			UsuarioSession usuarioSession,JstlLocalization local) {
 		this.result = result;
 		this.validator = validator;
 		this.publicoAlvoDAO = publicoAlvoDAO;
 		this.usuarioSession = usuarioSession;
+		this.local = local;
+		this.i18n = local.getBundle(local.getLocale());
 	}
 	@OpenTransaction
 	public void formulario() {
@@ -77,6 +82,7 @@ public class PublicoAlvoController {
 	public void apagar(Long id) {
 		try {
 			publicoAlvoDAO.apagarPorId(id);
+			result.include("success", i18n.getString("msg.success.apagar"));
 			result.redirectTo(this).listar();
 		} catch (Exception e) {
 			validator.add(new I18nMessage("Público Alvo", "msg.error.apagar")).onErrorRedirectTo(this).listar();
